9c6b4b5252dedc9c552493cf7aa963ea16151a34
sgc93 <smachewgedefaw@gmail.com>
2023-02-25 05:13:42 -0900

    views are updated

--- T-SQL/Views.sql
+++ T-SQL/Views.sql
@@ -6,7 +6,7 @@ Use Shumabo_secondary_school
 -- full information of Teachers
 
 GO
-    CREATE VIEW Teacher_data AS
+    CREATE VIEW Teacher_data.Teacher_data AS
     SELECT  Teacher_data.Teacher.Teacher_ID,
             Teacher_data.Teacher.F_name,
             Teacher_data.Teacher.L_name,
@@ -32,7 +32,7 @@ GO
 -- list of subjects and there Teachers
 
 GO
-    CREATE VIEW Subject_Teacher AS
+    CREATE VIEW Assignment.Subject_Teacher AS
     SELECT  Assignment.Subject.Subject_code AS Subject_code,
             Assignment.Subject.Subject_name AS Subject_name,
             Assignment.Teacher_section_subject.Section_code AS Section_code,
@@ -48,51 +48,51 @@ GO
 -- Submitted roasters
 
 GO
-    CREATE VIEW Submittable_roaster AS
-    SELECT  Roaster_list.Ac_year, 
-            Roaster_list.Stud_ID, 
-            Roaster_list.Grade_level_ID, 
-            Roaster_list.Section_code, 
-            Roaster_list.Subject_code, 
-            (Roaster_list.Frist_sem_mid_exam_25 + Roaster_list.First_sem_quiz_10 + Roaster_list.First_sem_assignment_15 + Roaster_list.First_sem_final_exam_50) AS First_sem_Sum,
-            (Roaster_list.Second_sem_mid_exam_25 + Roaster_list.Second_sem_quiz_10 + Roaster_list.Second_sem_assignment_15 + Roaster_list.Second_sem_final_exam_50) AS Second_sem_Sum
-    FROM Roaster_list
+    CREATE VIEW Stud_data.Submittable_roaster AS
+    SELECT  Stud_data.Roaster.Ac_year, 
+            Stud_data.Roaster.Stud_ID, 
+            Stud_data.Roaster.Grade_level_ID, 
+            Stud_data.Roaster.Section_code, 
+            Stud_data.Roaster.Subject_code, 
+            (Stud_data.Roaster.Frist_sem_mid_exam_25 + Stud_data.Roaster.First_sem_quiz_10 + Stud_data.Roaster.First_sem_assignment_15 + Stud_data.Roaster.First_sem_final_exam_50) AS First_sem_Sum,
+            (Stud_data.Roaster.Second_sem_mid_exam_25 + Stud_data.Roaster.Second_sem_quiz_10 + Stud_data.Roaster.Second_sem_assignment_15 + Stud_data.Roaster.Second_sem_final_exam_50) AS Second_sem_Sum
+    FROM Stud_data.Roaster
 GO
 
-SELECT * FROM Submittable_roaster
+SELECT * FROM Stud_data.Submittable_roaster
 
 -- view 4
 -- Current status of Items
 
 GO
-    CREATE VIEW Current_Item_Status AS
+    CREATE VIEW Resource.Current_Item_Status AS
     SELECT  Resource.All_item.ISer_no, 
             Resource.All_item.Item_name, 
             Resource.All_item.Item_type, 
             Resource.All_item.Unit_price,
             Resource.New_item.Recieving_date, 
             Resource.New_item.Added_quantity,
-            (Resource.New_item.Added_quantity - Resource.All_item.Currejant_quantity) AS Withdrew_quantity,
+            (Resource.New_item.Added_quantity - Resource.All_item.Current_quantity) AS Withdrew_quantity,
             Resource.All_item.Current_quantity,
             Resource.All_item.Total_price
     FROM Resource.All_item
     LEFT JOIN Resource.New_item ON Resource.All_item.ISer_no = Resource.New_item.ISer_no
-    LEFT JOIN Withdrawed_Items ON Resource.All_item.ISer_no = Withdrawed_Items.ISer_no;
+    LEFT JOIN Resource.Withdrew_Item ON Resource.All_item.ISer_no = Resource.Withdrew_Item.ISer_no;
 GO
 
 -- view 5
 -- containing the full information of the grade_levels
 
 GO
-    CREATE VIEW  Grade_level_data AS
-    SELECT  Roaster_list.Ac_year,
+    CREATE VIEW  Assignment.Grade_level_data AS
+    SELECT  Stud_data.Roaster.Ac_year,
             Assignment.Grade_level.Grade_level_ID,
             Assignment.Grade_level.Grade_level_name,
             Assignment.Section.Section_Code,
             Stud_data.Student.Stud_ID,
             Stud_data.Student.Gender
-    FROM Roaster_list
-    LEFT JOIN Assignment.Grade_level ON Roaster_list.Grade_level_ID = Assignment.Grade_level.Grade_level_ID
+    FROM Stud_data.Roaster
+    LEFT JOIN Assignment.Grade_level ON Stud_data.Roaster.Grade_level_ID = Assignment.Grade_level.Grade_level_ID
     LEFT JOIN Assignment.Section ON Assignment.Grade_level.Grade_level_ID = Assignment.Section.Grade_level_ID
     LEFT JOIN Stud_data.Student ON Assignment.Section.Section_code = Stud_data.Student.Section_code
 GO
@@ -121,26 +121,27 @@ GO
     LEFT JOIN Assignment.Subject ON Assignment.Class_Schedule.Subject_code = Assignment.Subject.Subject_code
     LEFT JOIN Teacher_data.Teacher ON Assignment.Class_Schedule.Teacher_ID = Teacher_data.Teacher.Teacher_ID
 GO
-drop view Assignment.Class_schedule_detail
+
+
 -- view 7
 -- formalized Report card from each student
 
 GO
-    CREATE VIEW Report_card AS
-    SELECT  Transcript.Ac_year,
-            Transcript.Stud_ID,
-            Transcript.Grade_level_ID,
+    CREATE VIEW Stud_data.Report_card AS
+    SELECT  Stud_data.Transcript.Ac_year,
+            Stud_data.Transcript.Stud_ID,
+            Stud_data.Transcript.Grade_level_ID,
             Stud_data.Student.F_name,
             Stud_data.Student.L_name,
             Stud_data.Student.M_name,
             Stud_data.Student.Gender,
             Stud_data.Student.Age,
-            Transcript.First_semester_avg,
-            Transcript.Second_semester_avg,
-            Transcript.Final_avg,
-            Transcript.Conduct
-    FROM Transcript
-    JOIN Stud_data.Student ON Transcript.Stud_ID = Stud_data.Student.Stud_ID
+            Stud_data.Transcript.First_semester_avg,
+            Stud_data.Transcript.Second_semester_avg,
+            Stud_data.Transcript.Final_avg,
+            Stud_data.Transcript.Conduct
+    FROM Assignment.Stud_data.Transcript
+    JOIN Stud_data.Student ON Stud_data.Transcript.Stud_ID = Stud_data.Student.Stud_ID
 GO
 
 SELECT * FROM Report_card
